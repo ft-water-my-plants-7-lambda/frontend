72b1804f569ec67c87ef85b0de90219f217ff31a
richad.97 <rcastillo2022@gmail.com>
2021-11-15 10:24:23 -0800

    created very basic login form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ export default function App() {
   return (
     <div>
       <h2>Water My Plants</h2>
+      <Login />
     </div>
-  )
+  );
+}
+
+function Login() {
+  return (
+    <form>
+      <h2>Login</h2>
+      <label>
+        Username:
+        <input name="username" />
+      </label>
+      <label>
+        Password:
+        <input name="password" />
+      </label>
+      <button>Submit</button>
+      <button>Forget Username?</button>
+    </form>
+  );
 }
