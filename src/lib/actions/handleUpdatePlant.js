8907c00b67a485709e33232a94b4800c8a991a73
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-15 12:57:10 -0600

    added handleUpdatePlant thunk function

---
+++ src/lib/actions/handleUpdatePlant.js
@@ -0,0 +1,18 @@
+import { startAPICall, updatePlants, handleError, endAPICall } from '.'
+import { services } from '../../services'
+
+export const handleUpdatePlant = (plant) => async (dispatch) => {
+  dispatch(startAPICall())
+
+  try {
+    const plants = await services.updatePlant(plant)
+
+    dispatch(updatePlants(plants))
+
+    return plants
+  } catch (err) {
+    dispatch(handleError({ error: 'delete', message: err.message }))
+  } finally {
+    dispatch(endAPICall())
+  }
+}
--- src/services/updatePlant.js
+++ src/services/updatePlant.js
@@ -1,3 +1,5 @@
 import axiosWithAuth from '../utils/axiosWithAuth'
 
-export const updatePlant = (plant) => null
+export const updatePlant = (plant) => {
+  const { id: plantId } = plant
+}
