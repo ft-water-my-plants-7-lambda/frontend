fd757244ac9ae1acd2e8e6b8d9782d2b84fa91d7
richad.97 <rcastillo2022@gmail.com>
2021-11-15 11:59:41 -0800

    moved registration form to its own file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
+import RegistrationForm from "./components/RegistrationForm";
+
 export default function App() {
   return (
     <div>
       <h2>Water My Plants</h2>
       <RegistrationForm />
     </div>
-  );
-}
-
-function RegistrationForm() {
-  return (
-    <form>
-      <h2>Registration</h2>
-      <label>
-        Username:
-        <input></input>
-      </label>
-      <label>
-        Phone Number:
-        <input></input>
-      </label>
-      <label>
-        Password:
-        <input></input>
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
   );
 }
---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,20 @@
+export default function RegistrationForm() {
+  return (
+    <form>
+      <h2>Registration</h2>
+      <label>
+        Username:
+        <input></input>
+      </label>
+      <label>
+        Phone Number:
+        <input></input>
+      </label>
+      <label>
+        Password:
+        <input></input>
+      </label>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
