d7fc627fe2bdfe475b761f1707fc3c1cedc162a6
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-15 11:40:57 -0600

    added reducer and actions

--- src/lib/reducer.js
+++ src/lib/reducer.js
@@ -1,1 +1,17 @@
-export const reducer = () => null
+import { ACTIONS } from './actions'
+
+const initialState = {
+  user: {
+    isAuthenticated: false,
+    username: '',
+  },
+  isLoading: false,
+  validationError: '',
+}
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
