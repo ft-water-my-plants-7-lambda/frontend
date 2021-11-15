f5884b3e563969322c332f153dc94360163df84c
richad.97 <rcastillo2022@gmail.com>
2021-11-15 10:35:20 -0800

    created very basic registration form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ export default function App() {
   return (
     <div>
       <h2>Water My Plants</h2>
+      <RegistrationForm />
     </div>
-  )
+  );
+}
+
+function RegistrationForm() {
+  return (
+    <form>
+      <h2>Registration</h2>
+      <label>
+        Username:
+        <input></input>
+      </label>
+      <label>
+        Phone Number:
+        <input></input>
+      </label>
+      <label>
+        Password:
+        <input></input>
+      </label>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
 }
