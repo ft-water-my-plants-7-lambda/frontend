cb63f433035f69b19c26737888b673e99b4e2f92
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-11-15 13:01:34 -0500

    landing page / plants components created

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 
 import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
+import Plants from './components/Plants/Dashboard'
+import AddPlants from './components/Plants/AddPlants'
 
 export default function App() {
   return (
     <>
       <Router>
         <Navbar />
-        {/* <Switch>
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          {/* <Route path="/register" component={Register} /> */}
+          {/* <Route path="/login" component={Login} /> */}
           <PrivateRoute path='/plants' component={Plants} />
           <PrivateRoute path='/plants/add' component={AddPlants} />
-          <PrivateRoute path='/plants/:id' component={Plant} />
-          <Route exact path="/" component={LandingPage} />
-        </Switch> */}
+          {/* <PrivateRoute path='/plants/:id' component={Plant} /> */}
+        </Switch>
       </Router>
     </>
   )
