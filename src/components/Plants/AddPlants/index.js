375f1d2c75f5ef122b3192c962e7e55883d82999
richad.97 <rcastillo2022@gmail.com>
2021-11-16 10:15:56 -0800

    added very basic add plant form

--- src/components/Plants/AddPlants/index.js
+++ src/components/Plants/AddPlants/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const Form = styled.form`
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  margin: 0 20%;
+  text-align: center;
+`;
 
 const AddPlants = () => {
   return (
     <>
-      <h1>Add Plants Form goes here</h1>
+      {/* <h1>Add Plants Form goes here</h1> */}
+      <Form>
+        <h2>Add Plants</h2>
+        <label>
+          Plant Name:
+          <input name="name" type="text" />
+        </label>
+        <label>
+          Plant Species:
+          <input name="species" type="text" />
+        </label>
+        <label>
+          H20 Frequency:
+          <input name="h20freq" type="text" />
+        </label>
+        <label>
+          Photo:
+          <button>Add Photo</button>
+        </label>
+        <button>Add Plant</button>
+      </Form>
     </>
   );
-}
+};
 
 export default AddPlants;
