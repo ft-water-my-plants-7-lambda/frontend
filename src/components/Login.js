197b928281f7b133d11408879120eee57bb865f5
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-16 11:18:52 -0600

    added Login services

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,21 @@
-import axios from "axios";
-import { useState } from "react";
+import axios from 'axios'
+import { useState } from 'react'
 
-const initialCredentials = { username: "", password: "" };
+const initialCredentials = { username: '', password: '' }
 
 export default function Login() {
-  const [credentials, setCredentials] = useState(initialCredentials);
+  const [credentials, setCredentials] = useState(initialCredentials)
 
   const change = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
+    const name = e.target.name
+    const value = e.target.value
 
-    setCredentials({ ...credentials, [name]: value });
-  };
+    setCredentials({ ...credentials, [name]: value })
+  }
 
   const submit = (e) => {
-    e.preventDefault();
-
-    axios
-      .post(
-        "https://water-my-plants-7-ft.herokuapp.com/api/users/login",
-        credentials
-      )
-      .then((resp) => {
-        console.log(resp);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    e.preventDefault()
+  }
 
   return (
     <form onSubmit={submit}>
@@ -43,5 +31,5 @@ export default function Login() {
       <button type="submit">Login</button>
       <button>Forgot Password?</button>
     </form>
-  );
+  )
 }
--- src/services/login.js
+++ src/services/login.js
@@ -1,3 +1,11 @@
 import axios from 'axios'
 
-export const login = (user) => null
+export const login = async (user) => {
+  try {
+    const res = await axios.post('https://water-my-plants-7-ft.herokuapp.com/api/users/login', user)
+
+    return res
+  } catch (err) {
+    return err
+  }
+}
