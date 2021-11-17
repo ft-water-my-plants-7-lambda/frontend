ab3654f4b5f438de4a9b5257b5091a8acefdde5f
richad.97 <rcastillo2022@gmail.com>
2021-11-17 06:30:29 -0800

    added more styling to forms

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -6,7 +6,7 @@ import { handleRegister } from "../lib/actions/handleRegister";
 
 import { H2, Form, Label, Input, Button } from "./FormStyledComponents";
 
-const initialCredentials = { username: "", /* phoneNumber: "",*/ password: "" };
+const initialCredentials = { username: "", phoneNumber: "", password: "" };
 
 const RegisterForm = ({ handleRegister }) => {
   const { push } = useHistory();
@@ -34,12 +34,16 @@ const RegisterForm = ({ handleRegister }) => {
         Username:
         <Input onChange={change} name="username" type="text" />
       </Label>
-      {/* <label>
+      <Label>
         Phone Number:
-        <input></input>
-      </label> */}
+        <Input onChange={change} name="username" type="tel" />
+      </Label>
       <Label>
         Password:
+        <Input onChange={change} name="password" type="text" />
+      </Label>
+      <Label>
+        Confirm Password:
         <Input onChange={change} name="password" type="text" />
       </Label>
       <Button type="submit">Sign Up</Button>
